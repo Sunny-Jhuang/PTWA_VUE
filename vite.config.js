53dcaefab89e4070d34b6c4034fe6a9f25f4b8e3
Sunny-Jhuang <90752081+Sunny-Jhuang@users.noreply.github.com>
2025-01-24 13:24:39 +0800

    修改vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/",
+  base: "/PTWA_VUE/",
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
